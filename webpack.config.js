1a8317abf572ba5b4de8ee16b2703b82f450361a
Carlos G <cguidog@gmail.com>
2019-05-08 06:32:11 -0400

    optimization bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,16 @@
 const path = require('path');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = (env) => {
     const isProduction = env === 'production';
-    
+    const CSSExtract = new ExtractTextPlugin('sytles.css');
     return {
         entry: './src/app.js',
         output: {
             path: path.join(__dirname, 'public'),
             filename: 'bundle.js'
         },
-        plugins: [new CopyWebpackPlugin([
-            {from:'src/resources/assets',to:'images'} 
-        ])],
         module: {
             rules: [{
                 loader: 'babel-loader',
@@ -20,11 +18,16 @@ module.exports = (env) => {
                 exclude: /node_modules/
             }, {
                 test: /\.css$/,
-                use: [
-                    'style-loader', 'css-loader'
-                ]
+                use: CSSExtract.extract({
+                    use: 'css-loader'
+                })
             }]
         },
+        plugins: [new CopyWebpackPlugin([
+            {from:'src/resources/assets',to:'images'} 
+        ]),
+        CSSExtract
+        ],
         devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public')
