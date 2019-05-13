147388e18bd875068cd43dbbe9d4fb3e2d5a92b1
Carlos G <cguidog@gmail.com>
2019-05-13 05:37:20 -0400

    removed contact link and added stars

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,6 @@ const Contact = () => {
         <div id='contact'>
             <div className='contact_container'>
                 <div><a href='https://github.com/cguidog' target='_blank'><i className="fab fa-github"></i></a></div>
-                <div><a><i className="fab fa-linkedin-in"></i></a></div>
             </div>
         </div>
     )
