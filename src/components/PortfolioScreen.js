5ce7ac0b135fdff9eb94baea4386e21d66fb41a9
Carlos <cguido@jakegroup.com>
2022-03-10 09:25:08 -0500

    updated projects

--- src/components/PortfolioScreen.js
+++ src/components/PortfolioScreen.js
@@ -20,12 +20,8 @@ export default class PortfolioScreen extends React.Component {
                     {title: 'Lácteos Don Joaquín', url: 'https://lacteosdonjoaquin.com/', image: '/images/don_joaquin.jpg', description: ['Website managed with WordPress.', 'SEO optimization.', 'Speed and Performance optimization.']}
                 ],
                 [
-                    {title: 'Portfolio Site', url: 'https://iamguido.com', image: '/images/portfolio.png', git: 'https://github.com/cguidog/portfolio_2019', description: ['The same website you are at right now.', 'Built with ReactJS.', 'Uses Babel, Express & Webpack.']},
-                    {title: 'React Car Search', url: 'https://react-car-search.herokuapp.com/', image: '/images/react_car_search.jpg', git: 'https://github.com/cguidog/carDisplay', description: ['Simple search tool created with React.','Built with ReactJS', 'Uses Babel, Express, Redux & Webpack.']},
-                    {title: 'ToDo List', url: 'https://cguidog.github.io/to-do-list/index.html', image: '/images/to_do_list.jpg', git: 'https://github.com/cguidog/to-do-list', description: ['A simple to-do app.', 'Built with Jquery.']},
-                    {title: 'Weather App', url: 'https://codepen.io/cguidog/full/LJPZry/', image: '/images/weather_app.jpg', git: 'https://github.com/cguidog/weather-app', description: ['Displays your local weather.', 'Built with Javascript.', 'Uses API, JSON.']},
-                    {title: 'Wikipedia Viewer', url: 'https://cguidog.github.io/wikipediaViewer/', image: '/images/wiki_viewer.jpg', git: 'https://github.com/cguidog/wikipediaViewer', description: ['Display 5 main results from a Wikipedia Search.', 'Built with Jquery.', 'Uses API, JSON']},
-                    {title: 'Circles', url: 'https://cguidog.github.io/circles/index.html', image: '/images/circles.jpg', git: 'https://github.com/cguidog/circles', description: ['Random circles generator. (Needs a physical keyboard to work)', 'Built with Javascript and the Paper.Js API.', 'Uses JSON.']},
+                    {title: 'Scales Buddy', url: 'https://scalesbuddy.com', image: '/images/scales_buddy.jpg', description: ['Practice and learn the scales on the freboard.', 'Built with ReactJS']},
+                    {title: 'Portfolio Site', url: 'https://www.iamguido.com', image: '/images/portfolio.jpg', git: 'https://github.com/cguidog/portfolio_2019', description: ['The same website you are at right now.', 'Built with ReactJS.', 'Uses Babel, Express, SVG & Webpack.']},
                     {title: 'RGB Color Guessing Game', url: 'https://cguidog.github.io/rgb-color-game/', image: '/images/rgb_game.jpg', git: 'https://github.com/cguidog/rgb-color-game', description: ['Test your RGB knowledge by picking the right color.','Built with Javascript.']}
                 ]
             ]
