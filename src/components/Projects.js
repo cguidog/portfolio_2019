6f6db276f131ecc397c61c8f8c9d4b7c8726034b
Carlos <cguido@jakegroup.com>
2022-03-10 09:50:36 -0500

    minor changes

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,15 +4,15 @@ import Details from './Details';
 const Projects = (props) => {
     if (props.selected === 0) {
         return (
-            <div className="project_display">{props.professional.map((project)=>{
+            <div className="project_display">{props.professional.map((project) => {
                 return (
                     <div key={props.professional.indexOf(project)} className="project_container">
                         <div className="project_image_container">
-                            {project.url ? <a href={project.url} target="_blank" rel="noopener"><img src={project.image}/></a> : <a><img src={project.image}/></a>}
+                            {project.url ? <a href={project.url} target="_blank" rel="noopener"><img src={project.image} /></a> : <a><img src={project.image} /></a>}
                         </div>
                         <div className="project_info">
-                            {project.url ? <a href={project.url} target="_blank" rel="noopener"><h2>{project.title}</h2></a> : <a><h2>{project.title}</h2></a>} 
-                            <Details description={project.description}/>
+                            {project.url ? <a href={project.url} target="_blank" rel="noopener"><h2>{project.title}</h2></a> : <a><h2>{project.title}</h2></a>}
+                            <Details description={project.description} />
                         </div>
                     </div>
                 )
@@ -20,11 +20,11 @@ const Projects = (props) => {
         );
     } else if (props.selected === 1) {
         return (
-            <div className="project_display">{props.personal.map((project)=> {
+            <div className="project_display">{props.personal.map((project) => {
                 return (
                     <div key={props.personal.indexOf(project)} className="project_container">
                         <div className="project_image_container">
-                            <a href={project.url} target="_blank" rel="noopener"><img src={project.image}/></a>
+                            <a href={project.url} target="_blank" rel="noopener"><img src={project.image} /></a>
                         </div>
                         <div className="project_info">
                             <div className="project_info_header">
@@ -32,10 +32,10 @@ const Projects = (props) => {
                                     <a href={project.url} target="_blank" rel="noopener"><h2>{project.title}</h2></a>
                                 </div>
                                 <div>
-                                    <a href={project.git} target="_blank" rel="noopener"><i className="fab fa-github"></i></a>
+                                    {project.git && <a href={project.git} target="_blank" rel="noopener"><i className="fab fa-github"></i></a>}
                                 </div>
                             </div>
-                            <Details description={project.description}/>
+                            <Details description={project.description} />
                         </div>
                     </div>
                 )
