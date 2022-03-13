ff6fa1b639db23857d4d2a2809af0c98a8707ef6
Carlos <cguido@jakegroup.com>
2022-03-13 15:54:35 -0400

    minor changes

--- src/components/PortfolioScreen.js
+++ src/components/PortfolioScreen.js
@@ -16,10 +16,10 @@ const PortfolioScreen = () => {
             <div id='portfolio' className={portfolio === 'professional' ? 'portfolio_professional' : 'portfolio_personal'}>
                 <div className="portfolio_title">
                     <div className={portfolio === 'professional' ? 'professional_title_selected' : 'professional_title'} onClick={handlePortfolio}>
-                        <h2>{portfolio === 'professional' ? 'Professional Projects' : 'Personal Projects'}</h2>
+                        <h2 id="title_label" >{portfolio === 'professional' ? 'Professional Projects' : 'Personal Projects'}</h2>
                     </div>
                     <div className={portfolio === 'professional' ? 'personal_title' : 'personal_title_selected'} onClick={handlePortfolio}>
-                        <h2>{portfolio === 'professional' ? 'Personal Projects' : 'Professional Projects'}</h2>
+                        <h2 id="title_label" >{portfolio === 'professional' ? 'Personal Projects' : 'Professional Projects'}</h2>
                     </div>
                 </div>
                 <div>
