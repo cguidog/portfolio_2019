20a6aac14b81fe84a97d2cd475aaa3d133fe87a5
Carlos <v-carlosgui@microsoft.com>
2023-05-19 07:12:44 -0400

    Updated content

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -27,7 +27,7 @@ const Skills = () =>
         <div className="star"></div>
         <div className="star"></div>
         <div className="description animated bounceInLeft delay-2s">I am a self-taught web developer who loves to create: from building furniture to designing clothes, from tiling kitchens to writing code.</div>
-        <div className="skills animated bounceInRight delay-2s">I build beautiful websites and web applications with a focus on user experience and accessibility. Check out the Projects section to learn more about my work.</div>
+        <div className="skills animated bounceInRight delay-2s">I build beautiful websites and web applications with a focus on user experience and accessibility. Check out the Projects section or my <a style={{'text-decoration':'underline'}} href="https://github.com/cguidog" target="_blank">Github profile</a> to learn more about my work.</div>
         <div className="trim_bottom animated bounceInLeft delay-2s">
             <div className="trim_white"></div>
             <div className="trim_block"></div>
