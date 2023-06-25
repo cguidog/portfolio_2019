2b88083506bf4449abc5906335f1884749982b8d
Carlos <v-carlosgui@microsoft.com>
2023-06-24 21:38:06 -0400

    Added Analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -6,6 +7,14 @@ export default function Document() {
       <Head>
       <link rel="icon" href="/favicon.png" type="image"/>
       <title>I am Guido</title>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-Y82ZSGRY20" />
+        <Script id="gtag" strategy="lazyOnload" dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'G-Y82ZSGRY20');`
+        }} />
       </Head>
       <body>
         <Main />
