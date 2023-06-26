3cb0ca86d8ccf5d55d267b394393506a3ebc4a5a
Carlos <v-carlosgui@microsoft.com>
2023-06-26 17:53:15 -0400

    Added analytics code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -9,13 +8,6 @@ export default function Document() {
         <title>I am Guido</title>
       </Head>
       <body>
-      <Script id="gtag_script" src="https://www.googletagmanager.com/gtag/js?id=G-Y82ZSGRY20" strategy="lazyOnload"></Script>
-        <Script id="gtag" strategy="lazyOnload">
-          {`window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-Y82ZSGRY20');`}
-        </Script>
         <Main />
         <NextScript />
       </body>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,18 +3,24 @@ import Footer from '@/components/Footer';
 import Hero from '@/components/Hero'
 import Projects from '@/components/Projects'
 import { Outfit } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script'
 
 const outfit = Outfit({subsets: ['latin']});
 
 export default function Home() {
   return (
     <main className={`${outfit.className}`}>
+            <Script id="gtag_script" src="https://www.googletagmanager.com/gtag/js?id=G-Y82ZSGRY20" strategy="lazyOnload"></Script>
+        <Script id="gtag" strategy="lazyOnload">
+          {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-Y82ZSGRY20');`}
+        </Script>
       <Hero />
       <Bio />
       <Projects />
       <Footer />
-      <Analytics />
     </main>
   )
 }
