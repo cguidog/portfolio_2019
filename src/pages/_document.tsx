822438e078f5a20d8a82080ecb40e4dac3f6cf34
Carlos <v-carlosgui@microsoft.com>
2023-06-26 16:08:29 -0400

    Added analytics code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,7 @@ export default function Document() {
         <link rel="icon" href="/favicon.png" type="image" />
         <title>I am Guido</title>
       </Head>
+      <body>
       <Script src="https://www.googletagmanager.com/gtag/js?id=G-Y82ZSGRY20" ></Script>
         <Script id="gtag" strategy="lazyOnload">
           {`window.dataLayer = window.dataLayer || [];
@@ -15,7 +16,6 @@ export default function Document() {
         gtag('js', new Date());
         gtag('config', 'G-Y82ZSGRY20');`}
         </Script>
-      <body>
         <Main />
         <NextScript />
       </body>
